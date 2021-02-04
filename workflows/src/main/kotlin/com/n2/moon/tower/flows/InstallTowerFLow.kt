9db21d58cfb83e7ce77940f52f477a69dd54834b
Rajesh <fun_rajesh@yahoo.com>
2021-02-04 19:42:01 +0000

    Initial commit

---
+++ workflows/src/main/kotlin/com/n2/moon/tower/flows/InstallTowerFLow.kt
@@ -0,0 +1,79 @@
+package com.n2.moon.tower.flows
+
+
+import co.paralleluniverse.fibers.Suspendable
+import com.n2.moon.tower.contracts.TowerContract
+import com.n2.moon.tower.states.TowerState
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+/**
+ * This is the flow which handles issuance of new Towers on the ledger.
+ * Gathering the counterparty's signature is handled by the [CollectSignaturesFlow].
+ * Notarisation (if required) and commitment to the ledger is handled by the [FinalityFlow].
+ * The flow returns the [SignedTransaction] that was committed to the ledger.
+ */
+@InitiatingFlow
+@StartableByRPC
+class InstallTowerFlow(val state: TowerState): FlowLogic<SignedTransaction>() {
+    @Suspendable
+    override fun call(): SignedTransaction {
+
+        // Step 1. Get a reference to the notary service on our network and our key pair.
+        // Note: ongoing work to support multiple notary identities is still in progress.
+
+        /**
+         *  METHOD 1: Take first notary on network, WARNING: use for test, non-prod environments, and single-notary networks only!*
+         *  METHOD 2: Explicit selection of notary by CordaX500Name - argument can by coded in flow or parsed from config (Preferred)
+         *
+         *  * - For production you always want to use Method 2 as it guarantees the expected notary is returned.
+         */
+        val notary = serviceHub.networkMapCache.notaryIdentities.single() // METHOD 1
+        // val notary = serviceHub.networkMapCache.getNotary(CordaX500Name.parse("O=Notary,L=London,C=GB")) // METHOD 2
+
+        // Step 2. Create a new issue command.
+        // Remember that a command is a CommandData object and a list of CompositeKeys
+        val issueCommand = Command(TowerContract.Commands.Issue(), state.participants.map { it.owningKey })
+
+        // Step 3. Create a new TransactionBuilder object.
+        val builder = TransactionBuilder(notary = notary)
+
+        // Step 4. Add the iou as an output state, as well as a command to the transaction builder.
+        builder.addOutputState(state, TowerContract.IOU_CONTRACT_ID)
+        builder.addCommand(issueCommand)
+
+        // Step 5. Verify and sign it with our KeyPair.
+        builder.verify(serviceHub)
+        val ptx = serviceHub.signInitialTransaction(builder)
+
+        val sessions = (state.participants - ourIdentity).map { initiateFlow(it) }.toSet()
+        // Step 6. Collect the other party's signature using the SignTransactionFlow.
+        val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
+
+        // Step 7. Assuming no exceptions, we can now finalise the transaction.
+        return subFlow(FinalityFlow(stx, sessions))
+    }
+}
+
+/**
+ * This is the flow which signs IOU issuances.
+ * The signing is handled by the [SignTransactionFlow].
+ */
+@InitiatedBy(InstallTowerFlow::class)
+class InstallTowerFlowResponder(val flowSession: FlowSession): FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val signedTransactionFlow = object : SignTransactionFlow(flowSession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "This must be an IOU transaction" using (output is TowerState)
+            }
+        }
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(otherSideSession = flowSession, expectedTxId = txWeJustSignedId.id))
+    }
+}
